157050d07d058c95faf5668ccb9c27f727721e27
seokjinjeong-cod <jsj961@naver.com>
2021-07-18 17:56:39 +0900

    java기초

---
+++ Chapter2/src/variable/Boolean.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class Boolean {
+
+	
+	public static void main(String[] args) {
+
+		boolean isMarried = false;
+		
+		System.out.println(isMarried);
+		
+		var i = 10.0;
+		System.out.println(i);
+	}
+
+}
--- Chapter2/src/variable/Variable.java
+++ Chapter2/src/variable/Variable.java
@@ -3,8 +3,13 @@ package variable;
 public class Variable {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int age, count;
+		
+		age = 10;
+		
+		System.out.println(age);
+		
+		int numberOfStudent;
+		
 	}
-
 }
