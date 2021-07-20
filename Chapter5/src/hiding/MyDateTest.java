9fbd5d120972722c8bc198f2b2f2b86a8721f422
seokjinjeong-cod <jsj961@naver.com>
2021-07-20 10:52:44 +0900

    정보 은닉

---
+++ Chapter5/src/hiding/MyDateTest.java
@@ -0,0 +1,16 @@
+package hiding;
+
+public class MyDateTest {
+
+	public static void main(String[] args) {
+
+		MyDate date = new MyDate();
+
+		date.setYear(2020);
+		date.setMonth(5234);
+		date.setDay(20);
+		
+		date.showDate();
+	}
+
+}
