58d47d98c740cf1f00920dd6aab70a503d627f68
seokjinjeong-cod <jsj961@naver.com>
2021-07-22 13:04:15 +0900

    상속, 오버라이딩, 형 변환, 다형성

---
+++ Chapter7/src/polymorphism/VIPCustomer.java
@@ -0,0 +1,31 @@
+package polymorphism;
+
+public class VIPCustomer extends Customer {
+	
+	double salesRatio;
+	private int agentID;
+	
+	public VIPCustomer(int customerID, String customerName, int agentID) {
+		super(customerID, customerName);
+		
+		customerGrade = "VIP";
+		bonusRatio = 0.05;
+		salesRatio = 0.1;
+		this.agentID = agentID;
+	}
+	
+	@Override
+	public int calcPrice(int price) {
+		bonusPoint += price * bonusRatio;
+		return price - (int)(price * salesRatio);
+	}
+
+	@Override
+	public String showCustomerInfo() {
+		return super.showCustomerInfo() + "담당 상담원 번호는" + agentID + "입니다.";
+	}
+
+	public int getAgentID() {
+		return agentID;
+	}
+}
