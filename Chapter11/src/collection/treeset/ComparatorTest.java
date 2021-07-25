19a41e02fd1d59001d9eaaeeb1db7780d9ac6ef7
seokjinjeong-cod <jsj961@naver.com>
2021-07-25 21:12:58 +0900

    Comparator

--- Chapter11/src/collection/treeset/ComparatorTest.java
+++ Chapter11/src/collection/treeset/ComparatorTest.java
@@ -4,7 +4,6 @@ import java.util.Comparator;
 import java.util.TreeSet;
 
 class MyCompare implements Comparator<String>{
-
 	@Override
 	public int compare(String o1, String o2) {
 		return o1.compareTo(o2)*(-1);
