6fbd74045b5fb299882662e50c8aa36803d9c13a
seokjinjeong-cod <jsj961@naver.com>
2021-07-26 13:31:19 +0900

    Comparator

---
+++ Chapter12/src/innerclass/InnerTest.java
@@ -0,0 +1,18 @@
+package innerclass;
+
+class OutClass{
+	
+	class InClass{
+		
+	}
+	
+}
+
+public class InnerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
