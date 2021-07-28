c160ae9ec6dc78a90eaceda8458e547f21afa434
seokjinjeong-cod <jsj961@naver.com>
2021-07-28 21:20:49 +0900

    데코레이터 패턴

---
+++ Chapter14/src/stream/coffee/KenyaAmericano.java
@@ -0,0 +1,9 @@
+package stream.coffee;
+
+public class KenyaAmericano extends Coffee {
+
+	@Override
+	public void brewing() {
+		System.out.print("KenyaAmericano ");
+	}
+}
---
+++ Chapter14/src/stream/coffee/Mocha.java
@@ -0,0 +1,17 @@
+package stream.coffee;
+
+public class Mocha extends Decorator {
+
+	public Mocha(Coffee coffee) {
+		super(coffee);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void brewing() {
+		super.brewing();
+		System.out.print("Adding Mocha Syrup ");
+	}
+	
+
+}
